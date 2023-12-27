e352ead6a015fe63d72246d196b59e1756d47c11
aarchiiive <wychrodjxz691@gmail.com>
2023-12-27 22:12:44 +0900

    golf pose estimation project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {}, 
+  },
+  variants: {
+    extend: {}, 
+  },
+  plugins: [],
+};
+
