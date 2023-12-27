19f789eab40906a7e397950a9d3095323f7ede7b
aarchiiive <wychrodjxz691@gmail.com>
2023-12-28 05:53:51 +0900

    Add home page route

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/home';
 import Record from './pages/record';
 import Loading from './pages/loading';
 
 ReactDOM.render(
   <Router>
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/record" element={<Record />} />
       <Route path="/loading" element={<Loading />} />
     </Routes>
