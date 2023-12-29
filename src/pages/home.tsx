5b09842aecb580fa9b66bb227d1d884da4237020
aarchiiive <wychrodjxz691@gmail.com>
2023-12-30 02:35:37 +0900

    Add typewriter effect and comment out company logo

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,11 +7,24 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <div className="home">
-      <button 
-      onClick={() => {
-        navigate('/record');
-      }}
+      <div className="typewriter">
+        <h1>H-Swing Project Arrived in CES 2024</h1>
+      </div>
+      <button
+        onClick={() => {
+          navigate('/record');
+        }}
       >Start</button>
+      {/* Company logo */}
+      {/* <div className="home-company-logo">
+        <a href="http://hurotics.com/" target="_blank" rel="noopener noreferrer">
+          <img
+            src={require('../assets/hurotics.png')}
+            alt="Company Logo"
+            className="company-logo"
+          />
+        </a>
+      </div> */}
     </div>
   );
 };
