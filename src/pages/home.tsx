6e6dfcc771858237eb476010220d1160d2df39ca
aarchiiive <wychrodjxz691@gmail.com>
2023-12-30 03:10:49 +0900

    Adjust font size and cursor color in typewriter component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,9 +5,28 @@ import '../styles/home.css';
 
 const Home = () => {
   const navigate = useNavigate();
+  const typewriterRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const adjustFontSize = () => {
+      if (typewriterRef.current) {
+        const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+        const fontSize = vw * 0.016; // 4% of viewport width
+        typewriterRef.current.style.fontSize = `${fontSize}px`;
+      }
+    };
+
+    // Adjust font size on mount and window resize
+    adjustFontSize();
+    window.addEventListener('resize', adjustFontSize);
+
+    // Cleanup listener
+    return () => window.removeEventListener('resize', adjustFontSize);
+  }, []);
+
   return (
     <div className="home">
-      <div className="typewriter">
+      <div className="typewriter" ref={typewriterRef}>
         <h1>H-Swing Project Arrived in CES 2024</h1>
       </div>
       <button
