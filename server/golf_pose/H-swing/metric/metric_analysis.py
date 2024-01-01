2e4d3b3a3670dcf62a0970dc5e4c057c97c020b2
ROKMC1250 <hjh1037@gmail.com>
2024-01-02 02:25:27 +0900

    return messages

--- server/golf_pose/H-swing/metric/metric_analysis.py
+++ server/golf_pose/H-swing/metric/metric_analysis.py
@@ -11,10 +11,13 @@ import os
 import argparse
 from scipy.stats import linregress
 from .metricc import *
+from collections import defaultdict
+import pprint
+import json
 
 class MetricAnalysis():
     def __init__(self, pro_path):
-        self.score = []
+        self.score_list = []
         self.weight_list = [1]*20
         self.weight_sum = np.sum(self.weight_list)
         self.final_sum = 0
@@ -22,21 +25,257 @@ class MetricAnalysis():
         self.pro_path = pro_path
         self.pro_list = []
         self.usr_list = []
+        self.pro = None
+        self.usr = None
+        self.idx = None
+        self.score = None
+        self.angle = None
+        self.mode = None
+        self.sequence = None
+        self.correction = defaultdict(dict)
+        self.pp = pprint.PrettyPrinter(indent=1, width= 160)
         for i in range(1,21):
             self.pro_name = 'metric_list' + str(i) + '.pickle'
             self.load_pro()
         self.img = 1
+
     def __call__(self, keypoints, event, left_start, right_start):
         return self.forward(keypoints, event, left_start, right_start)
     
     def forward(self, keypoints, event, left_start, right_start):
         self.load_usr(keypoints, event, left_start, right_start)
-        print(len(self.pro_list))
-        print(len(self.usr_list))
+        self.toe_up_analysis()
+        self.backswing_analysis()
+        self.top_analysis()
+        self.downswing_analysis()
+        self.impact_analysis()
+        self.finish_analysis()
+        return self.correction
+        # self.pp.pprint(self.correction)
+    
+    def toe_up_analysis(self):
+        self.sequence = 'toe up'
+        self.correction[self.sequence] = defaultdict(list)
+        self.idx = [0, 1, 2]
+        for i in self.idx:
+            self.get_score(i)
+            self.print_correction(i)
 
+    def backswing_analysis(self):
+        self.sequence = 'backswing'
+        self.correction[self.sequence] = defaultdict(list)
+        self.idx = [3, 4, 5, 17]
+        for i in self.idx:
+            self.get_score(i)
+            self.print_correction(i)
 
+    def top_analysis(self):
+        self.sequence = 'top'
+        self.correction[self.sequence] = defaultdict(list)
+        self.idx = [6, 7, 8]
+        for i in self.idx:
+            self.get_score(i)
+            self.print_correction(i)
 
+    def downswing_analysis(self):
+        self.sequence = 'downswing'
+        self.correction[self.sequence] = defaultdict(list)
+        self.idx = [9, 10, 11, 12, 18]
+        for i in self.idx:
+            self.get_score(i)
+            self.print_correction(i)
 
+    def impact_analysis(self):
+        self.sequence = 'impact'
+        self.correction[self.sequence] = defaultdict(list)
+        self.idx = [13, 14, 15, 16]
+        for i in self.idx:
+            self.get_score(i)
+            self.print_correction(i)
+
+    def finish_analysis(self):
+        self.sequence = 'finish'
+        self.correction[self.sequence] = defaultdict(list)
+        self.idx = [19]
+        for i in self.idx:
+            self.get_score(i)
+            self.print_correction(i)
+ 
+    def get_score(self, index):
+        if index in [17, 18]:
+            self.pro = int(self.pro_list[index])
+            self.usr = int(self.usr_list[index])
+            if self.pro > self.usr:
+                try:
+                    self.score = int((1 - ((self.pro-self.usr)/30))*100)
+                except:
+                    self.score = 0
+            else: 
+                self.score = 100
+        elif index in [16, 19]:
+            self.pro = int(self.pro_list[index])
+            self.usr = int(self.usr_list[index])
+            if self.pro >= self.usr:
+                self.score = 100
+            else:
+                try:
+                    self.score = int((1 - (self.usr-self.pro)/30)*100)
+                except:
+                    self.score = 0
+        else:
+            self.pro = round(self.pro_list[index],1)
+            self.usr = round(self.usr_list[index],1)
+            self.angle = self.usr - self.pro
+            if self.angle > 0 :
+                try:
+                    self.score = int((1-(self.angle/180))*100) if index in [0, 3, 6, 9] else int((1-(self.angle/45))*100)
+                except:
+                    self.score = 0
+                self.mode = 'higher'
+            elif self.angle < 0 :
+                self.angle = self.angle * -1
+                try:
+                    self.score = int((1-(self.angle/180))*100) if index in [0, 3, 6, 9] else int((1-(self.angle/45))*100)
+                except:
+                    self.score = 0
+                self.mode = 'lower'
+            else:
+                self.mode = 'perfect'
+
+    def print_correction(self, index):
+
+        if index == 0:
+            if self.mode == 'higher':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, straightness of your right arm is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your right arm more away.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your right arm much further away.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            elif self.mode == 'lower':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, straightness of your right arm is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your right arm much further away.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your right arm much further away.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, straightness of your right arm is perfect.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+
+        elif index in [3, 6, 9]:
+            if self.mode == 'higher':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, straightness of your left arm is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your left arm more away.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your left arm much further away.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            elif self.mode == 'lower':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, straightness of your left arm is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your left arm more away.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, keep your left arm much further away.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, straightness of your left arm is perfect.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+
+        elif index in [1, 4, 7, 10, 13]:
+            if self.mode == 'higher':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, position of your right shoulder is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, lower your right shoulder more.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, lower your right shoulder much more.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            elif self.mode == 'lower':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, position of your right shoulder is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, lift your right shoulder more.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, lift your right shoulder much more.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, position of your right shoulder is perfect.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+        
+        elif index in [2, 5, 8, 11, 14]:
+            if self.mode == 'higher':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, rotation of your upper body is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, reduce rotation of your upper body more.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, reduce rotation of your upper body much more.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            elif self.mode == 'lower':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, rotation of your upper body is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, rotate your upper body more.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, rotate your upper body much more.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, rotation of your upper body is perfect.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+
+        elif index in [12, 15]:
+            if self.mode == 'higher':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, rotation of your lower body is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, reduce rotation of your lower body more.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, reduce rotation of your lower body much more.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            elif self.mode == 'lower':
+                if self.score >= 90:
+                    self.correction[self.sequence]['messages'].append("During {}, rotation of your lower body is perfect.".format(self.sequence))
+                elif  50 < self.score < 90:
+                    self.correction[self.sequence]['messages'].append("During {}, rotate your lower body more.".format(self.sequence))
+                else:
+                    self.correction[self.sequence]['messages'].append("During {}, rotate your lower body much more.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, rotation of your lower body is perfect.".format(self.sequence))
+                self.correction[self.sequence]['scores'].append(self.score)
+
+        elif index == 16:
+            if self.score >= 90:
+                self.correction[self.sequence]['messages'].append("During {}, position of your head is perfect.".format(self.sequence))
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, keep your head position.".format(self.sequence))
+            self.correction[self.sequence]['scores'].append(self.score)
+
+        elif index == 17:
+            if self.score >= 90:
+                self.correction[self.sequence]['messages'].append("During {}, your body shifting is perfect .".format(self.sequence))
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, Keep your body from shifting to the right .".format(self.sequence))
+            self.correction[self.sequence]['scores'].append(self.score)
+        elif index == 18:
+            if self.score >= 90:
+                self.correction[self.sequence]['messages'].append("During {}, your body shifting is perfect .".format(self.sequence))
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, Keep your body from shifting to the left .".format(self.sequence))
+            self.correction[self.sequence]['scores'].append(self.score)
+        elif index == 19:
+            if self.score >= 90:
+                self.correction[self.sequence]['messages'].append("During {}, your body shifting is perfect .".format(self.sequence))
+            elif  50 < self.score < 90:
+                self.correction[self.sequence]['messages'].append("During {}, move your body to the left more.".format(self.sequence))
+            else:
+                self.correction[self.sequence]['messages'].append("During {}, move your body to the left much more.".format(self.sequence))
+            self.correction[self.sequence]['scores'].append(self.score)
 
     def load_pro(self):
         pro = pd.read_pickle(os.path.join(self.pro_path, self.pro_name))
@@ -90,80 +329,4 @@ class MetricAnalysis():
     #         self.final_sum += self.score[i] * self.weight_list[i]
     #     self.result = self.final_sum / self.weight_sum
     #     return self.result
-
-
-if __name__ == '__main__':
-    ma = MetricAnalysis()
-    event = [128, 137, 142, 157, 161, 165, 171, 192] #준혁4
-    keypoints = pd.read_pickle('junyuk4.pickle')
-    ma(keypoints, event)
-
-# def add_toe_upper_rotate_analysis(my_swing_metric_list_3,metric_list_3):
-
-#     my_rotate = my_swing_metric_list_3
-#     pro_rotate = metric_list_3
-#     # print(my_rotate)
-#     # print(pro_rotate)
-#     # my_rotate['left'][-1] = 80
-
-
-#     if my_rotate['left'][-1] > pro_rotate['left'][-1]:
-
-#         if my_rotate['left'][-1] <= 60:
-#             angle = my_rotate['left'][-1] - pro_rotate['left'][-1]
-#             toe_up_score = int((1-(angle/80))*100)
-
-#             score.append(toe_up_score)
-#             print('toe_up시 상체회전이 완벽합니다!')
-#             print('toe_up 점수:', toe_up_score)           
-        
-        
-#         elif 60 < my_rotate['left'][-1] <= 70:
-
-#             angle = my_rotate['left'][-1] - pro_rotate['left'][-1]
-#             toe_up_score = int((1-(angle/70))*100)
-
-#             score.append(toe_up_score)
-#             print('toe_up시 상체를 조금만 더 돌리세요!')
-#             print('toe_up 점수:', toe_up_score)
-        
-
-#         else :
-#             angle = my_rotate['left'][-1] - pro_rotate['left'][-1]
-#             toe_up_score = int((1-(angle/60))*100)     
-            
-#             score.append(toe_up_score)
-#             print('toe_up시 상체가 너무 돌지 않고 있습니다! 더 많이 돌리세요!') 
-#             print('toe_up 점수:', toe_up_score) 
-
-#     else:       
-
-#         if my_rotate['left'][-1] >= 40:
-#             angle = pro_rotate['left'][-1] - my_rotate['left'][-1]
-#             toe_up_score = int((1-(angle/90))*100)
-
-#             score.append(toe_up_score)
-#             print('toe_up시 상체회전이 완벽합니다!')
-#             print('toe_up 점수:', toe_up_score) 
-
-
-#         elif 30 <= my_rotate['left'][-1] < 40:
-
-#             angle = pro_rotate['left'][-1] - my_rotate['left'][-1]
-#             toe_up_score = int((1-(angle/70))*100)
-
-#             score.append(toe_up_score)
-#             print('toe_up시  상체를 조금만 덜 돌리세요')
-#             print('toe_up 점수:', toe_up_score)  
-
-
-#         else:
-#             angle = pro_rotate['left'][-1] - my_rotate['left'][-1]
-#             toe_up_score = int((1-(angle/60))*100)
-
-#             score.append(toe_up_score)
-#             print('toe_up시 상체가 너무 돌아갔습니다! 조금만 돌리세요!')
-#             print('toe_up 점수:', toe_up_score) 
- 
-#     print()
 
--- server/golf_pose/H-swing/run.py
+++ server/golf_pose/H-swing/run.py
@@ -2,6 +2,7 @@ from golfdb.golfdb import GolfDB
 from yolov8.yolomodel import YOLOModel
 from metric.metric_analysis import MetricAnalysis
 import time
+from collections import defaultdict
 
 golfdb = GolfDB()
 yolo = YOLOModel()
@@ -10,10 +11,12 @@ metric_analyis = MetricAnalysis('metric/pro')
 start_time = time.time()
 
 input_video = 'dataset/junyuk4.mp4'
-event = golfdb(input_video)
-keypoints, video = yolo(input_video, event)
+frames = golfdb(input_video)
+keypoints, video = yolo(input_video, frames)
 left_start, right_start = yolo.left_start, yolo.right_start
-metric_analyis(keypoints, event, left_start, right_start)
+correction = metric_analyis(keypoints, frames, left_start, right_start)
 
 end_time = time.time()
 print('inference_time:{}s'.format(int(end_time)-int(start_time)))
+
+
