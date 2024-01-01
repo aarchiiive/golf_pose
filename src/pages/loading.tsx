3fa8928091518175ec0fc005ccd2e558d4147c73
aarchiiive <wychrodjxz691@gmail.com>
2024-01-01 20:53:45 +0900

    Add animation to loading component

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { motion, AnimatePresence } from 'framer-motion';
 import { Bars } from 'react-loader-spinner';
 
 import '../styles/loading.css';
 
 const Loading = () => {
+  const [visibleAnimation, setVisibleAnimation] = useState("animateFadeIn");
   // useEffect(() => {
   //   const fetchData = async () => {
   //     await new Promise((resolve) => setTimeout(resolve, 3000)); // 3초간 로딩을 시뮬레이션
@@ -16,8 +18,45 @@ const Loading = () => {
   //   fetchData();
   // }, []);
 
+  useEffect(() => {
+    // setVisibleAnimation("animateFadeOut");
+  }, []);
+
+  const visibleVariants = {
+    containerFadeIn: {
+      x: '100vw',
+      opacity: 0,
+    },
+    containerFadeOut: {
+      x: '0',
+      opacity: 1,
+    },
+
+    animateFadeIn: {
+      x: '0',
+      opacity: 1,
+      transition: { 
+        duration: 0.8, 
+        ease: [0.22, 0.61, 0.36, 1]
+      }
+    },
+    animateFadeOut: {
+      x: '-100vw',
+      opacity: 0,
+      transition: { 
+        duration: 0.8, 
+        ease: [0.22, 0.61, 0.36, 1]
+      }
+    },
+  };
+
   return (
-    <div className="loading-container">
+    <motion.div
+      className="loading-container"
+      variants={visibleVariants}
+      initial="containerFadeIn"
+      animate={visibleAnimation}
+    >
       {/* <div className="loader"></div> */}
       {/* <RingLoader color="#b1b1b1" loading={true} size={100} /> */}
       {/* <BarLoader color="#b1b1b1" loading={true}/> */}
@@ -31,7 +70,7 @@ const Loading = () => {
         visible={true}
       />
       <div className="loading-text">Loading...</div>
-    </div>
+    </motion.div>
   );
 };
 
