3793d9fc90ec041223c88e8b6011a3fa005b298f
aarchiiive <wychrodjxz691@gmail.com>
2024-01-02 13:47:54 +0900

    Refactor video conversion logic and add video conversion method

--- server/golf_pose/golf_pose/views.py
+++ server/golf_pose/golf_pose/views.py
@@ -23,11 +23,12 @@ from .h_swing import (
 
 logger = logging.getLogger(__name__)
 
+
 class VideoUploadView(APIView):
     device = 'cpu'
     metric_path = 'golf_pose/h_swing/metric/pro'
     
-    def post(self, request): # 이메일, 비밀번호, 비디오 파일 
+    def post(self, request):
         start_time = time.time()
         video_file = request.FILES.get('video', None)
         if not video_file:
@@ -37,65 +38,73 @@ class VideoUploadView(APIView):
         file_path = os.path.join('uploads', file_name)
         default_storage.save(file_path, video_file)
         
+        converted_file_path = os.path.join('uploads', f"{uuid.uuid4()}_converted.mp4")
+        
+        self.convert_video(file_path, converted_file_path)
+        
         golfDB = GolfDB(device=self.device)
         yolo = YOLOModel(device=self.device)
         metric_analyis = MetricAnalysis(self.metric_path)
         
-        frames = golfDB(file_path)
-        keypoints, video = yolo(file_path, frames)
+        frames = golfDB(converted_file_path)
+        keypoints, video = yolo(converted_file_path, frames)
         left_start, right_start = yolo.left_start, yolo.right_start
         correction = metric_analyis(keypoints, frames, left_start, right_start)
-        
-        
-        # yolo = YOLO('yolov8l.pt')
-        # results = yolo(file_path, stream=True)
-        
-        # for res in results:
-        #     logger.info(res)
 
         logger.info(f"Finished! ({time.time() - start_time:.4f}s)")
         
-        # results = yolo(f'uploads/{file_name}')
-        
-        # yolo = YOLO(f"{uuid.uuid4()}.mp4")
-        
-        # metric
         # score, message, images
         # return Response({"score": score, "message": "Video uploaded successfully."}, status=status.HTTP_201_CREATED)
         return Response({"message": "Video uploaded successfully.", "file_name": file_name}, status=status.HTTP_201_CREATED)
     
-    
-class VideoToGifAPIView(APIView):
-    def post(self, request):
-        video_file = request.FILES.get('video', None)
-        if not video_file:
-            return Response({'error': 'No video file provided'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # 임시 파일 저장
-        file_name = f"{uuid.uuid4()}.webm"
-        file_path = os.path.join('uploads', file_name)
-        default_storage.save(file_path, video_file)
-
-        # FFmpeg를 사용하여 GIF로 변환
-        output_gif_name = f"{uuid.uuid4()}.gif"
-        output_gif_path = os.path.join('uploads', output_gif_name)
-
+    def convert_video(self, input_path, output_path):
+        command = [
+            'ffmpeg',
+            '-i', input_path,
+            '-c:v', 'libx264',
+            '-preset', 'fast',
+            '-crf', '22',
+            output_path
+        ]
         try:
-            subprocess.run([
-                'ffmpeg', '-i', default_storage.path(file_path), 
-                '-vf', 'scale=320:-1', '-t', '10', '-r', '10', 
-                default_storage.path(output_gif_path)
-            ], check=True)
-
-            # GIF 파일 제공
-            if default_storage.exists(output_gif_path):
-                with default_storage.open(output_gif_path, 'rb') as gif:
-                    return FileResponse(gif, as_attachment=True, filename=output_gif_name)
-            else:
-                return Response({'error': 'GIF conversion failed'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            os.remove(input_path)
+            return result
         except subprocess.CalledProcessError as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        finally:
-            # 임시 파일 정리
-            default_storage.delete(file_path)
-            default_storage.delete(output_gif_path)
+            print(f"An error occurred during video conversion: {e.stderr}")
+            raise e
+    
+# class VideoToGifAPIView(APIView):
+#     def post(self, request):
+#         video_file = request.FILES.get('video', None)
+#         if not video_file:
+#             return Response({'error': 'No video file provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+#         # 임시 파일 저장
+#         file_name = f"{uuid.uuid4()}.webm"
+#         file_path = os.path.join('uploads', file_name)
+#         default_storage.save(file_path, video_file)
+
+#         # FFmpeg를 사용하여 GIF로 변환
+#         output_gif_name = f"{uuid.uuid4()}.gif"
+#         output_gif_path = os.path.join('uploads', output_gif_name)
+
+#         try:
+#             subprocess.run([
+#                 'ffmpeg', '-i', default_storage.path(file_path), 
+#                 '-vf', 'scale=320:-1', '-t', '10', '-r', '10', 
+#                 default_storage.path(output_gif_path)
+#             ], check=True)
+
+#             # GIF 파일 제공
+#             if default_storage.exists(output_gif_path):
+#                 with default_storage.open(output_gif_path, 'rb') as gif:
+#                     return FileResponse(gif, as_attachment=True, filename=output_gif_name)
+#             else:
+#                 return Response({'error': 'GIF conversion failed'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#         except subprocess.CalledProcessError as e:
+#             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+#         finally:
+#             # 임시 파일 정리
+#             default_storage.delete(file_path)
+#             default_storage.delete(output_gif_path)
