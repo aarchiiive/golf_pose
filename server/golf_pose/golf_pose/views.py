742a045b40f51318aefaa9479809e7809f187175
aarchiiive <wychrodjxz691@gmail.com>
2024-01-03 18:25:03 +0900

    Refactor video upload and analysis code

--- server/golf_pose/golf_pose/views.py
+++ server/golf_pose/golf_pose/views.py
@@ -41,21 +41,33 @@ class VideoUploadView(APIView):
         
         self.convert_video(file_path, converted_file_path)
         
-        try:
-            golfDB = GolfDB(device=self.device)
-            yolo = YOLOModel(device=self.device)
-            metric_analyis = MetricAnalysis(self.metric_path)
+        golfDB = GolfDB(device=self.device)
+        yolo = YOLOModel(device=self.device)
+        metric_analyis = MetricAnalysis(self.metric_path)
+        
+        frames = golfDB(converted_file_path)
+        keypoints, video = yolo(converted_file_path, frames)
+        left_start, right_start = yolo.left_start, yolo.right_start
+        correction = metric_analyis(keypoints, frames, left_start, right_start)
             
-            frames = golfDB(converted_file_path)
-            keypoints, video = yolo(converted_file_path, frames)
-            left_start, right_start = yolo.left_start, yolo.right_start
-            correction = metric_analyis(keypoints, frames, left_start, right_start)
-        except Exception as e:
-            logger.error(f"Error occurred during inference: {e}")
-            return Response({"message": "Error occurred during inference."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        finally:
-            os.remove(converted_file_path)
-            logger.info(f"Finished! ({time.time() - start_time:.4f}s)")
+        # try:
+        #     golfDB = GolfDB(device=self.device)
+        #     yolo = YOLOModel(device=self.device)
+        #     metric_analyis = MetricAnalysis(self.metric_path)
+            
+        #     frames = golfDB(converted_file_path)
+        #     keypoints, video = yolo(converted_file_path, frames)
+        #     left_start, right_start = yolo.left_start, yolo.right_start
+        #     correction = metric_analyis(keypoints, frames, left_start, right_start)
+        # except Exception as e:
+        #     logger.error(f"Error occurred during inference: {e}")
+        #     return Response({"message": "Error occurred during inference."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        # finally:
+        #     os.remove(converted_file_path)
+        #     logger.info(f"Finished! ({time.time() - start_time:.4f}s)")
+            
+        os.remove(converted_file_path)
+        logger.info(f"Finished! ({time.time() - start_time:.4f}s)")
         
         # score, message, images
         # return Response({"score": score, "message": "Video uploaded successfully."}, status=status.HTTP_201_CREATED)
@@ -78,37 +90,3 @@ class VideoUploadView(APIView):
             print(f"An error occurred during video conversion: {e.stderr}")
             raise e
     
-# class VideoToGifAPIView(APIView):
-#     def post(self, request):
-#         video_file = request.FILES.get('video', None)
-#         if not video_file:
-#             return Response({'error': 'No video file provided'}, status=status.HTTP_400_BAD_REQUEST)
-
-#         # 임시 파일 저장
-#         file_name = f"{uuid.uuid4()}.webm"
-#         file_path = os.path.join('uploads', file_name)
-#         default_storage.save(file_path, video_file)
-
-#         # FFmpeg를 사용하여 GIF로 변환
-#         output_gif_name = f"{uuid.uuid4()}.gif"
-#         output_gif_path = os.path.join('uploads', output_gif_name)
-
-#         try:
-#             subprocess.run([
-#                 'ffmpeg', '-i', default_storage.path(file_path), 
-#                 '-vf', 'scale=320:-1', '-t', '10', '-r', '10', 
-#                 default_storage.path(output_gif_path)
-#             ], check=True)
-
-#             # GIF 파일 제공
-#             if default_storage.exists(output_gif_path):
-#                 with default_storage.open(output_gif_path, 'rb') as gif:
-#                     return FileResponse(gif, as_attachment=True, filename=output_gif_name)
-#             else:
-#                 return Response({'error': 'GIF conversion failed'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-#         except subprocess.CalledProcessError as e:
-#             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-#         finally:
-#             # 임시 파일 정리
-#             default_storage.delete(file_path)
-#             default_storage.delete(output_gif_path)
