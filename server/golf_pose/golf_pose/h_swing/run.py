2e566306a7c5cbf9fafdbe70db5cf341060acbbb
aarchiiive <wychrodjxz691@gmail.com>
2024-01-03 16:55:37 +0000

    Update run.py with new input video and modified GolfDB and YOLOModel initialization

--- server/golf_pose/golf_pose/h_swing/run.py
+++ server/golf_pose/golf_pose/h_swing/run.py
@@ -5,20 +5,15 @@ from golfdb.golfdb import GolfDB
 from yolo.yolomodel import YOLOModel
 from metric.metric_analysis import MetricAnalysis
 
-golfdb = GolfDB()
-yolo = YOLOModel()
+golfdb = GolfDB(mode='save_images')
+yolo = YOLOModel(mode= 'save_images')
 metric_analyis = MetricAnalysis('golf_pose/h_swing/metric/pro')
 
 start_time = time.time()
 
-input_video = 'dataset/junyuk4.mp4'
-frames, not_sorted = golfdb(input_video)
-
-if not_sorted == 0:
-    keypoints, video = yolo(input_video, frames, not_sorted)
-elif not_sorted == 1:
-    keypoints, video, frames = yolo(input_video, frames, not_sorted)
-
+input_video = 'dataset/pro.mp4'
+frames = golfdb(input_video) 
+keypoints, video, frames = yolo(input_video, frames)
 left_start, right_start = yolo.left_start, yolo.right_start
 correction = metric_analyis(keypoints, frames, left_start, right_start)
 
