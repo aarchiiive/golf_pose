654199a35033f7b162015b6bd47ae0f84f28343f
aarchiiive <wychrodjxz691@gmail.com>
2024-01-05 04:23:49 +0900

    Update poseDetection.tsx to include Results interface

--- src/components/poseDetection.tsx
+++ src/components/poseDetection.tsx
@@ -7,7 +7,7 @@ import React, {
   MutableRefObject,
 } from "react";
 
-import { Pose, POSE_CONNECTIONS } from "@mediapipe/pose";
+import { Pose, Results, POSE_CONNECTIONS } from "@mediapipe/pose";
 import { Camera } from "@mediapipe/camera_utils";
 import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
 
@@ -18,59 +18,89 @@ interface UsePoseDetectionProps {
 }
 
 
-const PoseDetection = ({ videoRef, canvasRef }: UsePoseDetectionProps) => {
-  const pose = new Pose({
-    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`,
-  });
+// export const PoseDetection = ({ videoRef, canvasRef }: UsePoseDetectionProps) => {
+//   const pose = new Pose({
+//     locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`,
+//   });
 
-  pose.setOptions({
-    modelComplexity: 1,
-    smoothLandmarks: true,
-    enableSegmentation: false,
-    smoothSegmentation: false,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5,
-  });
+//   pose.setOptions({
+//     modelComplexity: 1,
+//     smoothLandmarks: true,
+//     enableSegmentation: false,
+//     smoothSegmentation: false,
+//     minDetectionConfidence: 0.5,
+//     minTrackingConfidence: 0.5,
+//   });
 
-  pose.onResults((results) => {
-    const canvasElement = canvasRef.current;
-    if (canvasElement) {
-      const canvasCtx = canvasElement.getContext("2d");
-      if (canvasCtx) {
-        canvasCtx.save();
-        canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-        canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+//   pose.onResults((results) => {
+//     const canvasElement = canvasRef.current;
+//     if (canvasElement) {
+//       const canvasCtx = canvasElement.getContext("2d");
+//       if (canvasCtx) {
+//         canvasCtx.save();
+//         canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+//         canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
 
-        // Drawing logic
-        if (results.poseLandmarks) {
-          drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, { color: '#00FF00', lineWidth: 4 });
-          drawLandmarks(canvasCtx, results.poseLandmarks, { color: '#FF0000', lineWidth: 2 });
-        }
+//         // Drawing logic
+//         if (results.poseLandmarks) {
+//           drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS, { color: '#00FF00', lineWidth: 4 });
+//           drawLandmarks(canvasCtx, results.poseLandmarks, { color: '#FF0000', lineWidth: 2 });
+//         }
 
-        canvasCtx.restore();
-      }
-    }
-  });
+//         canvasCtx.restore();
+//       }
+//     }
+//   });
 
-  useEffect(() => {
-    if (videoRef.current) {
-      const camera = new Camera(videoRef.current, {
-        onFrame: async () => {
-          await pose.send({ image: videoRef.current });
-        },
-        width: 1280,
-        height: 720,
+//   useEffect(() => {
+//     if (videoRef.current) {
+//       const camera = new Camera(videoRef.current, {
+//         onFrame: async () => {
+//           await pose.send({ image: videoRef.current });
+//         },
+//         width: 1280,
+//         height: 720,
+//       });
+//       camera.start();
+//     }
+//   }, [videoRef, pose]);
+
+//   return (
+//     <>
+//       <video ref={videoRef} className="video" />
+//       <canvas ref={canvasRef} className="canvas" />
+//     </>
+//   )
+// };
+
+
+export const drawCanvas = (ctx: CanvasRenderingContext2D, results: Results) => {
+  const width = ctx.canvas.width;
+  const height = ctx.canvas.height;
+
+  console.log('width: ', width);  
+
+  ctx.save();
+  ctx.clearRect(0, 0, width, height);
+  // canvas의 좌우 반전
+  ctx.scale(-1, 1);
+  ctx.translate(-width, 0);
+  // capture image 그리기
+  ctx.drawImage(results.image, 0, 0, width, height);
+  // 손의 묘사
+  if (results.poseLandmarks) {
+    // 골격 묘사
+    for (const landmarks of results.poseLandmarks) {
+      drawConnectors(ctx, [landmarks], POSE_CONNECTIONS, {
+        color: "#CBCBCB",
+        lineWidth: 2,
       });
-      camera.start();
+      drawLandmarks(ctx, [landmarks], {
+        color: "#FFE69D",
+        lineWidth: 1,
+        radius: 2,
+      });
     }
-  }, [videoRef, pose]);
-
-  return (
-    <>
-      <video ref={videoRef} className="video" />
-      <canvas ref={canvasRef} className="canvas" />
-    </>
-  )
+  }
+  ctx.restore();
 };
-
-export default PoseDetection;
