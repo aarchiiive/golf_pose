d99c55ac547f99e000096af9734305425010c732
aarchiiive <wychrodjxz691@gmail.com>
2024-01-08 14:36:25 +0000

    Refactor imports and clean up code

--- server/golf_pose/golf_pose/h_swing/run.py
+++ server/golf_pose/golf_pose/h_swing/run.py
@@ -1,23 +1,32 @@
 import time
 import pprint
 
-from golfdb.golfdb import GolfDB
-from yolo.yolomodel import YOLOModel
-from metric.metric_analysis import MetricAnalysis
+from golfdb import GolfDB
+from yolo import YOLOModel
+from metric import MetricAnalysis
 
 # golfdb = GolfDB(mode='save_images')
-yolo = YOLOModel(mode='save_iamges')
+yolo = YOLOModel()
 metric_analyis = MetricAnalysis('golf_pose/h_swing/metric/pro')
 
 start_time = time.time()
 
 input_video = 'dataset/pro.mp4'
 # frames = golfdb(input_video) 
-keypoints, video, frames = yolo(input_video)
-left_start, right_start = yolo.left_start, yolo.right_start
-correction = metric_analyis(keypoints, frames, left_start, right_start)
+keypoints, frames = yolo(input_video)
+correction = metric_analyis(keypoints, frames, yolo.left_start, yolo.right_start)
 
 pp = pprint.PrettyPrinter(indent=2,width=160)
+print(yolo.save_path)
+
+
+### video
+# video = open(yolo.save_path, 'rb')
+
+
+print(yolo.event_list[1:6])
 pp.pprint(correction)
 end_time = time.time()
 print('inference_time:{}s'.format(int(end_time)-int(start_time)))
+
+
